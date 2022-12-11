23e2f0a05fb4fd53bbf401e0e978a6b95697cbb4
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-12-11 13:14:36 -0800

    port is now listening in server js

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,4 @@
-const express = require('express');
-
-const app = express();
+const notes = require('express').Router();
 //two API routes should be made: get /api/notes should read db.json and return all saved notes and JSOn
 // second route POST /api/notes should receive a new note to save on the request body, add it to db.json, 
 //and return new note to client Each note need unique id when save (npm packages could do this)
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/notes');
+const fs = require('fs');
+//const api = require('./routes/notes');
+//const htmlGet = require('./routes/html');
 
-const app = express();
+//process.env.port allows environment to designate port if necessary and since we're using heroku this is important?
 const PORT = process.env.PORT || 3001;
+const app = express();
 
+//doesn't load in browser 'unable to connect' worry about later?
+app.use(express.static('public'));
 
-module.exports = app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api);
+//app.use('/', htmlGet);
+
+app.listen(PORT, () => 
+console.log(`Server is listening at ${PORT}`)
+)
