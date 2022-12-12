742e7d6db97518b9da423f66d21fda93f990efc2
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-12-12 08:13:55 -0800

    notes post request now functional, adds object to db. Does not currently assign ID

--- routes/notes.js
+++ routes/notes.js
@@ -14,16 +14,32 @@ apiRouter.get('/notes', (req, res) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     })
-   /* fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) =>
-    {
-        if (err) throw err
-        res.json(JSON.parse(data))
-    })*/
-    //console.info(`${req.method} request received`);
-   //fs.readFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
-  // fs.readFile(path.join(__dirname, '../db.db.json').then((data) => res.json(JSON.parse)))
 } )
 
-//apiRouter.post
+apiRouter.post('/notes', (req, res) =>{
+    console.info(`${req.method} request recieved to add note`);
+    const { title, text } = req.body;
+    if (req.body){
+        const newNote = {
+            title,
+            text,
+            //need to assign it an id
+        };
+        fs.readFile(join(__dirname, '../db/db.json'), 'utf-8', (err, data) =>
+        {
+            if (err) throw err;
+            const notes = JSON.parse(data);
+            notes.push(newNote);
+            console.log(notes);
+            fs.writeFile(join(__dirname, '../db/db.json'),
+            JSON.stringify(notes),
+            (err) => { if (err) throw err;
+            console.info(`Data written to Notes Database`);
+            res.sendStatus(200);}
+            )
+        })
+    }
+}
+)
 
 module.exports = apiRouter
